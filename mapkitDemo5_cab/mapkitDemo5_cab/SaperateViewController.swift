36ea8fd3c821bba1f9c248622e7582ca739a6668
Manali-Mehta <manalitrt@gmail.com>
2020-04-13 15:49:03 +0530

    Function added

---
+++ mapkitDemo5_cab/mapkitDemo5_cab/SaperateViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SaperateViewController.swift
+//  mapkitDemo5_cab
+//
+//  Created by Third Rock Techkno on 13/04/20.
+//  Copyright © 2020 Third Rock Techkno. All rights reserved.
+//
+
+import UIKit
+
+class SaperateViewController: UIViewController {
+
+    @IBOutlet weak var lbl: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        lbl.text = "Saperate ViewController"
+    }
+    
+
+  
+
+}
